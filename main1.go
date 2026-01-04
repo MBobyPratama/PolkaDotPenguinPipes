39141955d1fdc868832f703febdb57eb06dd16d4
Muhammad Boby Pratama <bobyjakarta4@gmail.com>
2026-01-04 21:52:13 +0700

    040126

--- main1.go
+++ main1.go
@@ -5,14 +5,5 @@ importn "fmt"
 func main() {
 	var nama string = "Golang"
 	var umur int = 25
-	judul := "Belajar Golang"
-nilai := 90
-nilai := 100
-nilai := 110
-nilai := 120
-nilai := 130
-nilai := 140
-nilai := 150
-
-nilai := 160
+	var status bool = true
 }
